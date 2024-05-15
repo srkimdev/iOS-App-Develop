d5306b56df00a9b9f01809db52f1a8a21d2804f3
srkimdev <emirated3858@gmail.com>
2024-05-15 15:11:57 +0900

    0514 Assignment 2

---
+++ 0514_Assignment2_배달의 민족/Ethnic of Delivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ethnic of Delivery
+//
+//  Created by 김성률 on 5/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
