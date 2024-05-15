1ea8c371cc9d8305b27df67abb0fe029c1bd6feb
srkimdev <emirated3858@gmail.com>
2024-05-15 14:59:11 +0900

    0514 Assignment 1

---
+++ 0514_Assignment1_Netflix/Netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix
+//
+//  Created by 김성률 on 5/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
