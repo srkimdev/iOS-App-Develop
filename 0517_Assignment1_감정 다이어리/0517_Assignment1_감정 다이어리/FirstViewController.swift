8a4d1a9e8e4d8ce02f727b32d6482c115fbbf1e6
srkimdev <emirated3858@gmail.com>
2024-05-22 19:59:10 +0900

    0522 assignment using userdefault

--- 0517_Assignment1_감정 다이어리/0517_Assignment1_감정 다이어리/FirstViewController.swift
+++ 0517_Assignment1_감정 다이어리/0517_Assignment1_감정 다이어리/FirstViewController.swift
@@ -14,12 +14,20 @@ class FirstViewController: UIViewController {
     
     @IBOutlet var labelList: [UILabel]!
     
-    var count: [Int] = []
+    @IBOutlet var saveButton: UIButton!
+    @IBOutlet var resetButton: UIButton!
+    
+    var saveToggle: Bool = false
+    
+    var count: [Int] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     let emotion: [String] = ["행복해", "사랑해", "좋아해", "당황해", "속상해", "우울해", "심심해", "따분해", "걱정해"]
     var num: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        designButton(button: saveButton, buttonName: "저장", color: .blue)
+        designButton(button: resetButton, buttonName: "리셋", color: .red)
         
         view.backgroundColor = .brown
         
@@ -27,15 +35,15 @@ class FirstViewController: UIViewController {
         top_label.textColor = .black
         top_label.font = .systemFont(ofSize: 20)
         top_label.textAlignment = .center
-        
-        initialRandomNumber()
     
         for i in 0...8 {
+            count[i] = UserDefaults.standard.integer(forKey: "\(i)key")
+            
             designLabelName(emotionName: emotion[i], labelName: labelList[i], count: count[i])
         }
-        
     }
     
+    // 초기 랜덤으로 숫자 부여
     func initialRandomNumber() {
         var num: Int = 0
         
@@ -51,6 +59,12 @@ class FirstViewController: UIViewController {
         labelName.textColor = .black
     }
     
+    func designButton(button: UIButton, buttonName: String, color: UIColor) {
+        button.setTitle(buttonName, for: .normal)
+        button.setTitleColor(color, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 15)
+    }
+    
     @IBAction func buttonUp(_ sender: UIButton) {
         
         count[sender.tag] += 1
@@ -59,4 +73,19 @@ class FirstViewController: UIViewController {
     }
     
     
+    @IBAction func saveButtonClicked(_ sender: UIButton) {
+        
+        for i in 0...8 {
+            UserDefaults.standard.set(count[i], forKey: "\(i)key")
+        }
+        
+    }
+    
+    @IBAction func resetButtonClicked(_ sender: UIButton) {
+        for i in 0...8 {
+            count[i] = 0
+            designLabelName(emotionName: emotion[i], labelName: labelList[i], count: count[i])
+            UserDefaults.standard.set(0, forKey: "\(i)key")
+        }
+    }
 }
--- 0517_Assignment1_감정 다이어리/0517_Assignment1_감정 다이어리/SecondViewController.swift
+++ 0517_Assignment1_감정 다이어리/0517_Assignment1_감정 다이어리/SecondViewController.swift
@@ -16,11 +16,18 @@ class SecondViewController: UIViewController {
     
     @IBOutlet var buttonCollection: [UIButton]!
     
+    @IBOutlet var resetButton: UIButton!
+    
+    @IBOutlet var label1: UILabel!
+    @IBOutlet var label2: UILabel!
+    
     var new_word: [String] = ["꾸안꾸", "억까", "사바사", "어쩔티비", "뇌피셜", "금사빠", "낄끼빠빠", "심쿵", "레게노", "국룰"]
     
     let meaning: [String] = ["꾸민듯 안꾸민듯 자연스러운 모습", "억지로 까다", "사람 바이 사람", "어쩌라고 티비나 봐", "자기 머리에서 나온 생각이 사실이나 검증된 것 마냥 말하는 행위", "금방 사랑에 빠지는 사람", "낄 때 끼고 빠질 때 빠져라", "심장이 쿵쾅쿵쾅", "레전드", "국민 룰"]
     
     var num: Int = 0
+    
+    var recent: [String] = []
         
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +37,15 @@ class SecondViewController: UIViewController {
         textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15.0, height: 0.0))
         textField.leftViewMode = .always
         
+        label1.text = "최근 검색어"
+        label1.textColor = .black
+        label1.font = .systemFont(ofSize: 20)
+        label2.font = .systemFont(ofSize: 25)
+        label2.layer.borderColor = UIColor.black.cgColor
+        label2.layer.masksToBounds = true
+        label2.layer.borderWidth = 2
+        label2.layer.cornerRadius = 5
+        
         result.text = "검색결과가 없습니다."
         result.textColor = .black
         result.textAlignment = .center
@@ -38,6 +54,11 @@ class SecondViewController: UIViewController {
         
         magnitude.backgroundColor = .black
         magnitude.tintColor = .white
+        
+        resetButton.setTitle("리셋", for: .normal)
+        resetButton.setTitleColor(.red, for: .normal)
+        resetButton.titleLabel?.font = .systemFont(ofSize: 20)
+        
         
         for i in 0...3 {
             num = Int.random(in: 0...(9 - i))
@@ -79,6 +100,8 @@ class SecondViewController: UIViewController {
         
         new_word = ["꾸안꾸", "억까", "사바사", "어쩔티비", "뇌피셜", "금사빠", "낄끼빠빠", "심쿵", "레게노", "국룰"]
         
+        label2.text = label2.text! + " " + textField.text!
+        
     }
     
     @IBAction func buttonAll(_ sender: UIButton) {
@@ -86,5 +109,10 @@ class SecondViewController: UIViewController {
         result.text = meaning[new_word.firstIndex(of: buttonCollection[sender.tag].title(for: .normal)!)!]
         
     }
+    
+    @IBAction func resetButtonClicked(_ sender: UIButton) {
+        label2.text = ""
+    }
+    
 
 }
