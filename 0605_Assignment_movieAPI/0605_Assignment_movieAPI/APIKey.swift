2c05207a702cb15167456e162f753a4678ed9192
srkimdev <emirated3858@gmail.com>
2024-06-06 14:07:22 +0900

    add movie API gitignore

---
+++ 0605_Assignment_movieAPI/0605_Assignment_movieAPI/APIKey.swift
@@ -0,0 +1,14 @@
+//
+//  APIKey.swift
+//  0605_Assignment_movieAPI
+//
+//  Created by 김성률 on 6/6/24.
+//
+
+import Foundation
+
+struct APIKey {
+    
+    static let movieAPIKey = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=bcfed59bf390f812065251f405a4ac8c&targetDt="
+    
+}
--- 0605_Assignment_movieAPI/0605_Assignment_movieAPI/ViewController.swift
+++ 0605_Assignment_movieAPI/0605_Assignment_movieAPI/ViewController.swift
@@ -42,6 +42,8 @@ class ViewController: UIViewController {
         configureLayout()
         configureUI()
         
+        callRequest(date: "20120101")
+        
         searchButton.addTarget(self, action: #selector(searchButtonClicked), for: .touchUpInside)
         
     }
@@ -139,7 +141,7 @@ extension ViewController {
     
     func callRequest(date: String) {
         
-        let url = "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=bcfed59bf390f812065251f405a4ac8c&targetDt=\(date)"
+        let url = APIKey.movieAPIKey + date
         
         AF.request(url).responseDecodable(of: Welcome.self) { response in
             switch response.result {
