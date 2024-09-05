2ca4054cf4c9bf5448d757bd47087f09643082a4
srkimdev <emirated3858@gmail.com>
2024-09-05 17:14:50 +0900

    add CategoryView

---
+++ SwiftUIBasic/SwiftUIBasic/Category/CategoryView.swift
@@ -0,0 +1,72 @@
+//
+//  CategoryView.swift
+//  SwiftUIBasic
+//
+//  Created by 김성률 on 9/5/24.
+//
+
+import SwiftUI
+
+struct Categories: Hashable, Identifiable {
+    var id = UUID()
+    let name: String
+    let count: Int
+}
+
+struct CategoryView: View {
+    
+    @State private var text = ""
+    
+    let item = ["SF", "Animation", "Thriller", "Romance"]
+    
+    @State var list = [
+        Categories(name: "SF", count: 27)
+    ]
+    
+    var body: some View {
+        
+        NavigationView {
+            List {
+                
+                ForEach(list, id: \.id) { item in
+                    rowView(genre: item.name, num: item.count)
+                }
+                
+                
+            }
+            .navigationTitle("영화 검색")
+            .searchable(text: $text, prompt: "영화를 검색해보세요.")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        list.append(Categories(name: item.randomElement()!, count: .random(in: 1...100)))
+                    }, label: {
+                        Text("추가")
+                    })
+                }
+            }
+        }
+        
+        
+    }
+    
+    func rowView(genre: String, num: Int) -> some View {
+        
+        HStack {
+            Image(systemName: "person")
+            Text("\(genre)(\(num))")
+            Spacer()
+            Button {
+                
+            } label: {
+                Image(systemName: "chevron.right")
+                    .foregroundStyle(.gray)
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    CategoryView()
+}
